1519b2ca8d64812a140efde317c98bbf7800567e
Łukasz Bartulski <l.bartulski@gmail.com>
2026-03-21 12:25:26 +0100

    testbase and actionbot fixes

--- src/test/java/core/TestBase.java
+++ src/test/java/core/TestBase.java
@@ -2,23 +2,18 @@ package core;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class TestBase {
     protected WebDriver driver;
-    private WebDriverWait wait;
     protected ActionBot bot;
 
     protected final String baseURL = "https://www.saucedemo.com";
-    protected final String username = "standard_user";
-    protected final String password = "secret_sauce";
 
 
     @BeforeEach
@@ -33,13 +28,6 @@ public class TestBase {
         driver = new ChromeDriver(options);
         bot = new ActionBot(driver, baseURL);
         driver.get(baseURL);
-
-        driver.findElement(By.cssSelector("#user-name"))
-                .sendKeys(username);
-        driver.findElement(By.cssSelector("#password"))
-                .sendKeys(password);
-        driver.findElement(By.cssSelector("#login-button"))
-                .click();
     }
 
     @AfterEach
